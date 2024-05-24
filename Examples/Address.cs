4beb92477996433e6b6f4d900f9c91b3e12ba2de
d42y <phong007@gmail.com>
2024-05-24 10:20:34 -0500

    Add files via upload

---
+++ Examples/Address.cs
@@ -0,0 +1,20 @@
+﻿using IoTDBdotNET.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+        [TableForeignKey(typeof(Friend), TableConstraint.Cascading, RelationshipOneTo.One, "Each friend only have one address." )]
+        public Guid FriendId { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
